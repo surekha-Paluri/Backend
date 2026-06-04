9e20735ecc4059bf6a53f6dbf838d380bcc4c64d
surekha <lakshmisurekha.pitchapati@gmail.com>
2026-06-05 00:56:47 +0530

    practice

---
+++ python/datatypes.py
@@ -0,0 +1,24 @@
+# Data Types in Python
+# 1. Numeric Types
+# int, float, complex
+#example on int
+x = 5
+print(type(x))
+#example on float
+y = 5.0
+print(type(y))
+#example on complex
+z = 2 + 3j
+print(type(z))
+
+
+#string
+s = "Hello, World!"
+print(type(s))
+print(s[2])
+print(s[-1])
+#slicing
+print(s[0:5])
+s=s[0:5] + s[7:12]
+print(s)
+print(len(s))
